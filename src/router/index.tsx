113ccd1e1da08f7c9e6b17cfe60ff71d8a133f5f
Ivan Adithya <ivanjayasooriya03@gmail.com>
2026-05-18 14:52:30 +0530

    add routing

---
+++ src/router/index.tsx
@@ -0,0 +1,31 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// pages
+const Home = lazy(() => import("../pages/Home"));
+const Register = lazy(() => import("../pages/Register"));
+const Login = lazy(() => import("../pages/Login"));
+
+// router
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
